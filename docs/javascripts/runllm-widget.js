7f4166297008366231e06295c2f04447adc265d5
Andre Giron <andre@spiralai.co>
2024-05-16 13:43:28 -0700

    Working configuration for RunLLM widget.

--- docs/javascripts/runllm-widget.js
+++ docs/javascripts/runllm-widget.js
@@ -1,19 +1,17 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     var script = document.createElement('script');
-//     script.defer = true;
-//     script.type = 'module';
-//     script.id = 'runllm-widget-script';
-//     script.src = 'https://cdn.jsdelivr.net/npm/@runllm/search-widget@0.0.7-quix-1/dist/run-llm-search-widget.es.js';
-//     script.setAttribute('version', '0.0.7-quix-1');
-//     script.setAttribute('runllm-preset', 'mkdocs');
-//     script.setAttribute('runllm-server-address', 'https://api.runllm.com');
-//     script.setAttribute('runllm-assistant-id', '111');
-//     script.setAttribute('runllm-position', 'TOP_RIGHT');
-//     script.setAttribute('runllm-keyboard-shortcut', 'Mod+j');
-//     script.setAttribute('runllm-slack-community-url', 'https://quix.io/slack-invite');
-//     script.setAttribute('runllm-name', 'RunLLM');
+document.addEventListener("DOMContentLoaded", function() {
+    var script = document.createElement('script');
+    script.defer = true;
+    script.type = 'module';
+    script.id = 'runllm-widget-script';
+    script.src = 'https://cdn.jsdelivr.net/npm/@runllm/search-widget@stable/dist/run-llm-search-widget.es.js';
+    script.setAttribute('version', 'stable');
+    script.setAttribute('runllm-preset', 'mkdocs');
+    script.setAttribute('runllm-server-address', 'https://api.runllm.com');
+    script.setAttribute('runllm-assistant-id', '111');
+    script.setAttribute('runllm-position', 'TOP_RIGHT');
+    script.setAttribute('runllm-keyboard-shortcut', 'Mod+j');
+    script.setAttribute('runllm-slack-community-url', 'https://quix.io/slack-invite');
+    script.setAttribute('runllm-name', 'RunLLM');
 
-//     document.head.appendChild(script);
-// });
-
-console.log('hello from runllm widget no op script');
+    document.head.appendChild(script);
+});
